67f9deed87f88ac9a7231628a3e276013c728f19
RangoLiu <lyy_0816@163.com>
2021-08-17 23:42:14 +0800

    添加密码加密功能

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -7,8 +7,10 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationException;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.util.ByteSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +32,20 @@ public class UserController {
     UserService userService;
 
     /**
+     * 实现密码加密
+     *
+     * @param account 账号
+     * @param password 密码
+     * @return md5加密后的密码
+     */
+    String getMd5Password(String account,String password){
+        //盐，通过账号得到
+        ByteSource salt=ByteSource.Util.bytes(account);
+        Object md5Password= new SimpleHash("md5",password,salt);
+        return md5Password.toString();
+    }
+
+    /**
      * 实现注册功能
      *
      * @param avatar 头像
@@ -46,7 +62,8 @@ public class UserController {
             @RequestParam("username") String username,
             @RequestParam("account") String account,
             @RequestParam("password") String password) throws SQLException, IOException {
-        userService.register(avatar,username,account,password);
+        String md5Password=getMd5Password(account,password);
+        userService.register(avatar,username,account,md5Password);
     }
 
     /**
@@ -63,7 +80,8 @@ public class UserController {
             @RequestParam("password") String password){
         //用户认证信息
         Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken usernamePasswordToken=new UsernamePasswordToken(account,password);
+        String md5Password=getMd5Password(account,password);
+        UsernamePasswordToken usernamePasswordToken=new UsernamePasswordToken(account,md5Password);
         try{
             subject.login(usernamePasswordToken);
         }
@@ -79,6 +97,7 @@ public class UserController {
             log.warn("没有权限");
             return 4;
         }
+        log.warn("成功登录");
         return 1;
     }
 }
--- backend/src/main/java/com/example/backend/shiro/MyRealm.java
+++ backend/src/main/java/com/example/backend/shiro/MyRealm.java
@@ -5,8 +5,10 @@ import com.example.backend.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
 
 import javax.annotation.Resource;
 
@@ -15,14 +17,28 @@ public class MyRealm extends AuthorizingRealm {
     @Resource
     private UserService userService;
 
+
+    /**
+     * 授权
+     *
+     * @param principalCollection 身份集合
+     * @return 授权信息
+     */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         return null;
     }
 
+    /**
+     * 认证
+     *
+     * @param authenticationToken 令牌
+     * @return 认证信息
+     */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         UsernamePasswordToken usernamePasswordToken= (UsernamePasswordToken) authenticationToken;
+        //读取数据库的数据
         User user = userService.login(usernamePasswordToken.getUsername());
         if(user!=null){
             return new SimpleAuthenticationInfo(user,user.getPassword(),getName());
